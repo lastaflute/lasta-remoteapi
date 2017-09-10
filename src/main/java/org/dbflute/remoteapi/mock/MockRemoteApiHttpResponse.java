de65c4222b137ec5321ecf0b56a8974ca4172e2e
jflute <dbflute@gmail.com>
2017-09-10 15:04:11 +0900

    add mock

---
+++ src/main/java/org/dbflute/remoteapi/mock/MockRemoteApiHttpResponse.java
@@ -0,0 +1,182 @@
+/*
+ * Copyright 2015-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.dbflute.remoteapi.mock;
+
+import java.io.IOException;
+import java.util.Locale;
+
+import org.apache.http.Header;
+import org.apache.http.HeaderIterator;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpVersion;
+import org.apache.http.ProtocolVersion;
+import org.apache.http.StatusLine;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.message.BasicStatusLine;
+import org.apache.http.params.HttpParams;
+
+/**
+ * @author awane
+ * @author jflute
+ */
+@SuppressWarnings("all")
+public class MockRemoteApiHttpResponse implements CloseableHttpResponse {
+
+    protected final HttpEntity httpEntity;
+
+    public MockRemoteApiHttpResponse(HttpEntity httpEntity) {
+        this.httpEntity = httpEntity;
+    }
+
+    @Override
+    public HeaderIterator headerIterator(String name) {
+        return null;
+    }
+
+    @Override
+    public HeaderIterator headerIterator() {
+        return null;
+    }
+
+    @Override
+    public ProtocolVersion getProtocolVersion() {
+        return HttpVersion.HTTP_1_1;
+    }
+
+    @Override
+    public HttpParams getParams() {
+        return null;
+    }
+
+    @Override
+    public Header getLastHeader(String name) {
+        return null;
+    }
+
+    @Override
+    public Header[] getHeaders(String name) {
+        return null;
+    }
+
+    @Override
+    public Header getFirstHeader(String name) {
+        return null;
+    }
+
+    @Override
+    public Header[] getAllHeaders() {
+        return null;
+    }
+
+    @Override
+    public boolean containsHeader(String name) {
+        return false;
+    }
+
+    @Override
+    public StatusLine getStatusLine() {
+        return new BasicStatusLine(getProtocolVersion(), 200, "OK");
+    }
+
+    @Override
+    public Locale getLocale() {
+        return null;
+    }
+
+    @Override
+    public HttpEntity getEntity() {
+        return this.httpEntity;
+    }
+
+    @Override
+    public void close() throws IOException {
+    }
+
+    @Override
+    public void addHeader(String name, String value) {
+        throw new UnsupportedOperationException("not supported by mock.");
+    }
+
+    @Override
+    public void addHeader(Header header) {
+        throw new UnsupportedOperationException("not supported by mock.");
+    }
+
+    @Override
+    public void setStatusLine(ProtocolVersion ver, int code, String reason) {
+        throw new UnsupportedOperationException("not supported by mock.");
+    }
+
+    @Override
+    public void setStatusLine(ProtocolVersion ver, int code) {
+        throw new UnsupportedOperationException("not supported by mock.");
+    }
+
+    @Override
+    public void setStatusLine(StatusLine statusline) {
+        throw new UnsupportedOperationException("not supported by mock.");
+    }
+
+    @Override
+    public void setStatusCode(int code) throws IllegalStateException {
+        throw new UnsupportedOperationException("not supported by mock.");
+    }
+
+    @Override
+    public void setReasonPhrase(String reason) throws IllegalStateException {
+        throw new UnsupportedOperationException("not supported by mock.");
+    }
+
+    @Override
+    public void setLocale(Locale loc) {
+        throw new UnsupportedOperationException("not supported by mock.");
+    }
+
+    @Override
+    public void setEntity(HttpEntity entity) {
+        throw new UnsupportedOperationException("not supported by mock.");
+    }
+
+    @Override
+    public void setParams(HttpParams params) {
+        throw new UnsupportedOperationException("not supported by mock.");
+    }
+
+    @Override
+    public void setHeaders(Header[] headers) {
+        throw new UnsupportedOperationException("not supported by mock.");
+    }
+
+    @Override
+    public void setHeader(String name, String value) {
+        throw new UnsupportedOperationException("not supported by mock.");
+    }
+
+    @Override
+    public void setHeader(Header header) {
+        throw new UnsupportedOperationException("not supported by mock.");
+    }
+
+    @Override
+    public void removeHeaders(String name) {
+        throw new UnsupportedOperationException("not supported by mock.");
+    }
+
+    @Override
+    public void removeHeader(Header header) {
+        throw new UnsupportedOperationException("not supported by mock.");
+    }
+}
